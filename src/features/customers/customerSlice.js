6f4553cc7f36e2b39302a8a0c972b70716676fb4
generalentropy <entropie@tuta.io>
2024-01-18 14:02:50 +0100

    update

---
+++ src/features/customers/customerSlice.js
@@ -0,0 +1,39 @@
+const initialStateCustomer = {
+  fullName: "",
+  nationalID: "",
+  createdAt: "",
+};
+
+export default function customerReducer(state = initialStateCustomer, action) {
+  switch (action.type) {
+    case "customer/createCustomer":
+      return {
+        ...state,
+        fullName: action.payload.fullName,
+        nationalID: action.payload.nationalID,
+        createdAt: action.payload.createdAt,
+      };
+
+    case "customer/updateName":
+      return { ...state, fullName: action.payload };
+
+    default:
+      return state;
+  }
+}
+
+export function createCustomer(fullName, nationalID) {
+  const date = new Date().toISOString();
+
+  return {
+    type: "customer/createCustomer",
+    payload: { fullName, nationalID, createdAt: date },
+  };
+}
+
+export function updateName(fullName) {
+  return {
+    type: "account/updateName",
+    payload: fullName,
+  };
+}
