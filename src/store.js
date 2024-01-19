923a0b1bb468aa9c45db20a53b8b8947bc3c75c6
generalentropy <entropie@tuta.io>
2024-01-19 15:41:32 +0100

    update

--- src/store.js
+++ src/store.js
@@ -1,5 +1,8 @@
 import { applyMiddleware, combineReducers, createStore } from "redux";
 import { thunk } from "redux-thunk";
+// https://www.npmjs.com/package/@redux-devtools/extension
+import { composeWithDevTools } from "@redux-devtools/extension";
+
 import accountReducer from "./features/account/accountSlice";
 import customerReducer from "./features/customers/customerSlice";
 
@@ -8,6 +11,10 @@ const rootReducer = combineReducers({
   customer: customerReducer,
 });
 
-const store = createStore(rootReducer, applyMiddleware(thunk));
+const store = createStore(
+  rootReducer,
+
+  composeWithDevTools(applyMiddleware(thunk))
+);
 
 export default store;
